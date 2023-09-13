3c1832c522652b20b7cc1155b0c8223d27fb73a9
Tristan White <113216091+up2code3@users.noreply.github.com>
2023-09-13 16:54:03 -0400

    deleted a comment

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 const today = new Date();
 
 const thisYear = today.getFullYear();
@@ -36,76 +34,56 @@ for (i = 0; i < skills.length; i += 1) {
 }
 
 //const messageForm = document.getElementsByName('leave_message')[0];
-const messageForm = document.querySelector('form[name="leave_message"]') 
+const messageForm = document.querySelector('form[name="leave_message"]');
 
-messageForm.addEventListener('submit', (e) => {
+messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
- 
-    const usersName = e.target.usersName.value;
-    const usersEmail = e.target.usersEmail.value;
-    const usersMessage = e.target.usersMessage.value;
-    console.log(usersName,usersEmail,usersMessage);
-    
-    const messageSection = document.getElementById('messages');
-    let messageList = messageSection.querySelector('ul');
-    let newMessage = document.createElement('li');
-    newMessage.innerHTML = `<a href=mailto:${usersEmail}> ${usersName} </a>` + "wrote:" + `<span> ${usersMessage}</span>`
 
-    messageList.appendChild(newMessage);
-    let removeButton = document.createElement('button');
-    removeButton.innerText = 'remove';
-    removeButton.type='button';
+  const usersName = e.target.usersName.value;
+  const usersEmail = e.target.usersEmail.value;
+  const usersMessage = e.target.usersMessage.value;
+  console.log(usersName, usersEmail, usersMessage);
 
-    newMessage.appendChild(removeButton);
-    removeButton.addEventListener('click', (e) => {
-      let entry = removeButton.parentNode;
-      entry.remove();
+  const messageSection = document.getElementById("messages");
+  let messageList = messageSection.querySelector("ul");
+  let newMessage = document.createElement("li");
+  newMessage.innerHTML =
+    `<a href=mailto:${usersEmail}> ${usersName} </a>` +
+    "wrote:" +
+    `<span> ${usersMessage}</span>`;
 
-    })
+  messageList.appendChild(newMessage);
+  let removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
 
-    messageForm.reset();
+  newMessage.appendChild(removeButton);
+  removeButton.addEventListener("click", (e) => {
+    let entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  messageForm.reset();
 });
 
 let githubRequest = new XMLHttpRequest();
 
-githubRequest.open("GET","https://api.github.com/users/up2code3/repos")
+githubRequest.open("GET", "https://api.github.com/users/up2code3/repos");
 githubRequest.send();
 
 addEventListener("load", (Event) => {
   let repositories = JSON.parse(githubRequest.responseText);
-  console.log(repositories)
+  console.log(repositories);
 
+  // I compared the next to lines of code to the message section above to make sure it was right
+  let projectSection = document.getElementById("projects");
+  let projectList = projectSection.querySelector("ul"); // this queries just the project section and not the whole page
 
-// I compared the next to lines of code to the message section above to make sure it was right 
-let projectSection = document.getElementById("projects") 
-let projectList = projectSection.querySelector("ul") // this queries just the project section and not the whole page  
+  for (var i = 0; i < repositories.length; i++) {
+    let project = document.createElement("li");
+    project.innerText = repositories[i].name;
+    projectList.appendChild(project);
+  }
+});
 
-
-for (var i = 0 ; i < repositories.length ; i++){
-  let project = document.createElement("li");  
-  project.innerText = repositories[i].name; 
-  projectList.appendChild(project);
-}
-})
-
-//  I was stuck on line 87 for awhile, it had 7 li under the 
-//  projects element on the page, but they all said object object,
-//  so eventually it dawned on me to do the .name to refer 
-//  to the key of the pair. but that didnt work, so i tired name:,
-//  i thought maybe the colon was aprat of the name, it through 
-//  an error, so I deleted the colon leaving it as .name
-//  and suddenly it worked... weird... , even as i was typing this 
-//  message all of the li's under projects disappeared, and 
-//  I thought i broke it again, then I did a refresh and they came back. 
-// this is the error message it threw when they all randomly disappear 
-// 
-//  VM2188:1 Uncaught SyntaxError: Unexpected end of JSON input
-//     at JSON.parse (<anonymous>)at index.js:75:27</anonymous>
-//
-// then I just refresh and there is no longer an error, I think 
-//  I had this code right a few Times , but I would see the error,
-// and think it was broke, so I would try something else. I guess 
-//  i was just moving to fast? or maybe the autosave and auto refresh feature is 
-// making things Weird , or does AJAX just casue funny behavior?
-//  jeeez it broke after I type this message, and it took 3 refresh to get it back.
-// as of right now I see all the projects listed, I am going to save and commit.
+g
